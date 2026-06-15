62739241720ab969b32e115edb538de15c076f92
Akash230405 <akash.a3440@gmail.com>
2026-06-15 14:20:58 +0530

    Added candidate details, resume download, analytics and MongoDB Atlas integration

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
-import os
 
 client = MongoClient(
-    os.getenv("MONGO_URL")
+    "mongodb+srv://resumeadmin:ResumeAI123@cluster0resumeaicluster.wwfdp72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0ResumeAICluster"
 )
+
+try:
+    client.admin.command("ping")
+    print("✅ MongoDB Atlas Connected")
+except Exception as e:
+    print("❌ MongoDB Error:", e)
 
 db = client["resume_ai"]
 
